55f931e3bbabcf7064b15c478ec7215e8f5127ac
Nataliia Levak <nataliialevak@gmail.com>
2022-12-26 22:55:11 +0300

    Add files via upload

--- Lesson04task4.java
+++ Lesson04task4.java
@@ -1,13 +1,12 @@
 public class Lesson04task4 {
     public static void main(String[] args) {
-        int[] num1 = new int[] {1,2,3,4,5,6,7,8,9,10};
-        int[] num2 = {1, 2, 3, 4, 5, 6 ,7, 8, 9, 10};
+        int[] num1 = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int i = 0;
         int j = 0;
-        int multi = 0;
+        int multi;
         while (i < 10) {
             while (j < 10) {
-                multi = num1[i] * num2[j];
+                multi = num1[i] * num1[j];
                 System.out.printf("%d ", multi);
                 j++;
             }
