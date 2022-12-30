9e178c059fcfe39f2ba516b41996d5fb29a6fef4
Nataliia Levak <nataliialevak@gmail.com>
2022-12-30 18:40:35 +0300

    Add files via upload

--- Lesson04task3.java
+++ Lesson04task3.java
@@ -1,22 +1,21 @@
 public class Lesson04task3 {
     // Используя while, вывести в Standard Output первых 10 чисел Фибоначчи.
     public static void main(String[] args) {
-        int i = 0;
-        int j = 1;
-        int numFibonacci;
-        System.out.println("  10 Чисел Фибоначчи:");
         String textFib = "Число Фибоначчи";
-
+        System.out.println("  10 Чисел Фибоначчи:");
         System.out.printf("1. %s  0 %n", textFib);
         System.out.printf("2. %s  1 %n", textFib);
 
-        int n = 3;
-        while (n < 11) {
-            numFibonacci = i + j;
-            i = j;
-            j = numFibonacci;
-            System.out.printf("%d. %s %d %n", n, textFib, numFibonacci);
-            n++;
+        int firstNumFib = 0;
+        int secondNumFib = 1;
+        int counter = 3;
+        int numFibonacci;
+        while (counter < 11) {
+            numFibonacci = firstNumFib + secondNumFib;
+            firstNumFib = secondNumFib;
+            secondNumFib = numFibonacci;
+            System.out.printf("%d. %s %d %n", counter, textFib, numFibonacci);
+            counter++;
         }
     }
 }
